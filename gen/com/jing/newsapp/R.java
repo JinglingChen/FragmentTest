522f59f2348290b6996b375216384ac72662e5e7
jingling <1120881650@qq.com>
2015-06-07 21:30:03 +0800

    2015.06.08修改

--- gen/com/jing/newsapp/R.java
+++ gen/com/jing/newsapp/R.java
@@ -27,14 +27,15 @@ public final class R {
         public static final int spilt_line_vertical=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080002;
-        public static final int left_fragment=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int button1=0x7f080003;
         public static final int news_content=0x7f080006;
-        public static final int news_content_fragment=0x7f080003;
+        public static final int news_content_fragment=0x7f080002;
+        public static final int news_content_layout=0x7f080001;
         public static final int news_title=0x7f080005;
-        public static final int right_fragment=0x7f080001;
-        public static final int text_view=0x7f080007;
+        public static final int news_title_fragment=0x7f080000;
+        public static final int news_title_list_view=0x7f080007;
+        public static final int text_view=0x7f080008;
         public static final int visibility_layout=0x7f080004;
     }
     public static final class layout {
--- src/com/jing/newsapp/NewsTitleFragment.java
+++ src/com/jing/newsapp/NewsTitleFragment.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
@@ -20,6 +25,36 @@ public class NewsTitleFragment extends Fragment implements OnItemClickListener{
 		newsList=getNews();
 		adapter= new NewsAdapter(activity,R.layout.news_item,newsList);
 		
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+		View view=inflater.inflate(R.layout.news_titile_frag, container,false);
+		newsTitleListView =(ListView) view.findViewById(R.id.news_title_list_view);
+		newsTitleListView.setAdapter(adapter);
+		newsTitleListView.setOnItemClickListener(this);
+		return view;
+		
+	}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState){
+		super.onActivityCreated(savedInstanceState);
+		if(getActivity().findViewById(R.id.news_content_layout)!=null){
+			isTwoPane=true; //如果找到news_content_layout布局，则为双页模式
+		}else {
+			isTwoPane=false;
+		}
+	}
+	@Override
+	public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+		News news=newsList.get(position);
+		if(isTwoPane){
+			NewsContentFragment newsContentFragment = (NewsContentFragment) 
+					getFragmentManager().findFragmentById(R.id.news_content_fragment);
+			newsContentFragment.refresh(news.getTitle(), news.getContent());
+		}
+		else{
+			NewsContentActivity.actionStart(getActivity(), news.getTitle(), news.getContent());
+		}
 	}
 	
 	private List<News> getNews(){
